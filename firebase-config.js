38999ef552fa8e1d13dc981318f877bd870bcbbd
foxstorehaiti <foxstorehaiti@gmail.com>
2026-04-10 22:02:37 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB-v3pX9y8z7w6v5u4t3s2r1q0p",
+    authDomain: "pay-fusion-8feqg.firebaseapp.com",
+    projectId: "pay-fusion-8feqg",
+    storageBucket: "pay-fusion-8feqg.appspot.com",
+    messagingSenderId: "945593776812",
+    appId: "1:945593776812:web:4475e1085fdfa31690fd09",
+    measurementId: "G-8FEQG6B7ZY"
+};
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
